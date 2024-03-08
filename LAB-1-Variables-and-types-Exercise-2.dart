811d8e3e5077392c6fec1e219809223c6443e54f
iamsamienda <samuelendale7373@gmail.com>
2024-03-08 16:45:21 +0300

    Define a constant variable for the speed of light (299,792,458 meters per second). Calculate the time it takes for light to travel a certain distance (user input)

--- LAB-1-Variables-and-types-Exercise-2.dart
+++ LAB-1-Variables-and-types-Exercise-2.dart
@@ -1,7 +1,18 @@
-const speed = 299,792,458;
+import 'dart:io';
 
-void main(){
-    var distance = input("input distance")
-    var time = distance / speed;
-    print(time)
+const double speed = 299792458;
+
+void main() {
+  print('Enter the distance');
+  final distanceinput = stdin.readLineSync();
+
+  if (distanceinput != null && double.tryParse(distanceinput) != null) {
+
+    final distance = double.parse(distanceinput);
+    final time = distance / speed;
+    
+    print(time);
+  } else {
+    print('Invalid input');
+  }
 }
