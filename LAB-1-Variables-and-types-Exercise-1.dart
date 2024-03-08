79e7a7e62f52308ed0f8ba71c1f8f85af19740f9
iamsamienda <samuelendale7373@gmail.com>
2024-03-08 16:47:29 +0300

    Create variables to store your name, age, and favorite color, and print them to the console using string interpolation

--- LAB-1-Variables-and-types-Exercise-1.dart
+++ LAB-1-Variables-and-types-Exercise-1.dart
@@ -2,6 +2,6 @@ var name = 'samuel';
 var age = 22;
 var favoritecolor = 'black';
 
-void main(){
-    print('my name is $name, am $age years old , my favorite color is $favoritecolor')
+void main() {
+  print('my name is $name, am $age years old , my favorite color is $favoritecolor');
 }
